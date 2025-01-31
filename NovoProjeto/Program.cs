431ec21ebf0353b1ca7215a3587ff55d8ff531ff
R4FISX <rafaelrodrgues002@gmail.com>
2025-01-31 09:15:27 -0300

    Adicionar arquivos de projeto.

---
+++ NovoProjeto/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//ScreenSound
+String Menssagem = "Boas Vindas";
+String Linha = "**********";
+
+Console.WriteLine(Linha);
+Console.WriteLine(Menssagem);
+Console.WriteLine(Linha);
